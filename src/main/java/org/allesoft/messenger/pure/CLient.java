95309d19fa73cc679d16db56b5bef98d47407bf5
Kirill Abramovich <kirill.abramovich@magenta-technology.com>
2016-11-18 17:12:57 +0400

    packeted ping pong

--- src/main/java/org/allesoft/messenger/pure/CLient.java
+++ src/main/java/org/allesoft/messenger/pure/CLient.java
@@ -1,8 +1,13 @@
 package org.allesoft.messenger.pure;
 
+import org.abstractj.kalium.NaCl;
+import org.abstractj.kalium.crypto.Box;
 import org.abstractj.kalium.encoders.Hex;
+import org.abstractj.kalium.keys.PublicKey;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.security.SecureRandom;
 
 /**
  * Created by kabramovich on 15.11.2016.
@@ -20,19 +25,16 @@ public class Client {
             client.socket = new Socket(address, port);
             System.out.println("Client started " + client.socket.toString());
             new Thread(() -> {
-                try {
-                    byte[] buf = new byte[1024];
-                    while (true) {
-                        int c = client.socket.getInputStream().read(buf, 0, buf.length);
-                        if (c < 0) {
-                            return;
-                        }
+                while (true) {
+                    try {
+                        byte[] buf;
+                        buf = Server.waitPacketWithDBSizeHeader(client.socket.getInputStream());
                         System.out.println("Received by " + client.socket.toString());
                         System.out.println(Hex.HEX.encode(buf));
-                        client.socket.getOutputStream().write(buf, 0, c);
+                        Server.sendPacket(client.socket.getOutputStream(), buf);
+                    } catch (Exception e) {
+                        System.out.println("Exception: " + e);
                     }
-                } catch (Exception e) {
-                    System.out.println("Exception: " + e);
                 }
             }).start();
             return client;
@@ -41,11 +43,25 @@ public class Client {
         }
     }
 
-    public void sendPacket(byte[] packet) {
-        try {
-            socket.getOutputStream().write(packet);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void encryptExpensiveAndSendWithKeys(Box box, PublicKey our, PublicKey their,
+                                                byte[] payload) throws IOException {
+        byte[] nonce = new byte[NaCl.Sodium.NONCE_BYTES];
+        SecureRandom rng = new SecureRandom();
+        rng.nextBytes(nonce);
+        byte[] ourKey = our.toBytes();
+        byte[] theirKey = their.toBytes();
+        byte[] cryptoBody = box.encrypt(nonce, payload);
+        byte[] packet = new byte[nonce.length + ourKey.length + theirKey.length + cryptoBody.length];
+        int pos = 0;
+        System.arraycopy(nonce, 0, packet, 0, nonce.length);
+        System.arraycopy(ourKey, 0, packet, pos += nonce.length, ourKey.length);
+        System.arraycopy(theirKey, 0, packet, pos += ourKey.length, theirKey.length);
+        System.arraycopy(cryptoBody, 0, packet, pos + theirKey.length, cryptoBody.length);
+        Server.sendPacket(socket.getOutputStream(), packet);
     }
+
+    public void sendPacket(byte[] packet) throws Exception {
+        Server.sendPacket(socket.getOutputStream(), packet);
+    }
+
 }
