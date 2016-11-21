bd2bf82f7ffce897fa4defe1f13982b1fb790c72
kirill <kirill@hata>
2016-11-21 21:39:42 +0400

    checks

--- src/test/java/org/allesoft/messenger/UITest.java
+++ src/test/java/org/allesoft/messenger/UITest.java
@@ -1,5 +1,6 @@
 package org.allesoft.messenger;
 
+import org.allesoft.messenger.jclient.RosterItem;
 import org.allesoft.messenger.swingui.SwingUI;
 import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.Robot;
@@ -8,6 +9,9 @@ import org.fest.swing.finder.WindowFinder;
 import org.fest.swing.fixture.FrameFixture;
 import org.fest.swing.launcher.ApplicationLauncher;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Created by kabramovich on 21.11.2016.
@@ -21,16 +25,29 @@ public class UITest {
         Robot robot = BasicRobot.robotWithCurrentAwtHierarchy();
         FrameMatcher matcher = FrameMatcher.withName("mainWin");
         FrameMatcher addContactMatcher = FrameMatcher.withName("addWin");
+
         FrameFixture fixture = WindowFinder.findFrame(matcher).using(robot);
         fixture.button("connectButton").click();
-        Thread.sleep(1000l);
         fixture.button("addContactButton").click();
-        Thread.sleep(1000l);
-        fixture = WindowFinder.findFrame(addContactMatcher).using(robot);
-        fixture.textBox("userIdField").enterText("new user");
-        Thread.sleep(1000l);
-        fixture.button("addContactDoneButton").click();
 
-        for (;;);
+        FrameFixture addFixture = WindowFinder.findFrame(addContactMatcher).using(robot);
+        addFixture.textBox("userIdField").setText("");
+        addFixture.textBox("userIdField").enterText("new user");
+        assertTrue(addFixture.component().isVisible());
+        addFixture.button("addContactDoneButton").click();
+        //addFixture.requireNotVisible();
+        assertTrue(!addFixture.component().isVisible());
+
+        //assertEquals(fixture.table().cell("new user").toString(), "new user");
+        fixture.table().requireCellValue(fixture.table().cell("new user"), "new user");
+        assertEquals(
+                ((RosterItem)(
+                        fixture
+                                .table()
+                                .component()
+                                .getModel()
+                                .getValueAt(7, 1)))
+                        .getValue(),
+                "new user");
     }
 }
