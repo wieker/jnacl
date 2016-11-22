3a5cd0f707ee4c23e96767e4e51cbc91fb3ab88e
kirill <kirill@hata>
2016-11-22 21:11:12 +0400

    first layered test

--- src/test/java/org/allesoft/messenger/BottomUpTest.java
+++ src/test/java/org/allesoft/messenger/BottomUpTest.java
@@ -3,6 +3,7 @@ package org.allesoft.messenger;
 import org.abstractj.kalium.crypto.Box;
 import org.abstractj.kalium.encoders.Hex;
 import org.abstractj.kalium.keys.KeyPair;
+import org.allesoft.messenger.pure.CryptoLayer;
 import org.allesoft.messenger.pure.Server;
 import org.testng.annotations.Test;
 
@@ -21,6 +22,22 @@ public class BottomUpTest {
         org.allesoft.messenger.pure.Client c1 = org.allesoft.messenger.pure.Client.connectClient(box, "localhost", 1055);
         org.allesoft.messenger.pure.Client c2 = org.allesoft.messenger.pure.Client.connectClient(box, "localhost", 1055);
         c1.encryptExpensiveAndSendWithKeys(pair1.getPublicKey(), pair2.getPublicKey(), Hex.HEX.decode("FF77"));
+
+        for (;;) {
+
+        }
+    }
+
+    @Test
+    public void testY() throws Exception {
+        KeyPair pair1 = new KeyPair();
+        KeyPair pair2 = new KeyPair();
+        Box box = new Box(pair2.getPublicKey(), pair1.getPrivateKey());
+
+        Server server = Server.initServer(1055);
+        CryptoLayer l1 = new CryptoLayer("localhost", 1055, box, pair1.getPublicKey(), pair2.getPublicKey());
+        CryptoLayer l2 = new CryptoLayer("localhost", 1055, box, pair2.getPublicKey(), pair1.getPublicKey());
+        l1.sendPacket(Hex.HEX.decode("FF77"));
 
         for (;;) {
 
